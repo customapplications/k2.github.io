fbf7628c3ba15e6d1504d81b0def02dd74cf37e8
customapplications <r.celesti@mapei.it>
2025-12-10 12:04:49 +0100

    Update rtm.js

--- rtm.js
+++ rtm.js
@@ -1,19 +1,21 @@
 $("span[name^=MapeiCell]").css("padding", "10px 15px");
 
-function setFavicons(favImg){
+function setFavicons(favImg,size,type,rel){
     let headTitle = document.querySelector('head');
     let setFavicon = document.createElement('link');
-    setFavicon.setAttribute('rel','shortcut icon');
+    setFavicon.setAttribute('rel',rel);
     setFavicon.setAttribute('href',favImg);
+    if (size){
+        setFavicon.setAttribute('size',size);
+    }
+    if (type){
+        setFavicon.setAttribute('type',type);
+    }        
     headTitle.appendChild(setFavicon);
 }
-setFavicons('https://customapplications.github.io/k2.github.io/favicon/favicon.ico');
 
-/*
-<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
-<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-<link rel="shortcut icon" href="/favicon.ico" />
-<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-<link rel="manifest" href="/site.webmanifest" />
-
-*/
+setFavicons('https://customapplications.github.io/k2.github.io/favicon/favicon-96x96.png','96x96',null,'icon');
+setFavicons('https://customapplications.github.io/k2.github.io/favicon/favicon.svg',null,'image/svg+xml','icon');
+setFavicons('https://customapplications.github.io/k2.github.io/favicon/favicon.ico',null,null,'shortcut icon');
+setFavicons('https://customapplications.github.io/k2.github.io/favicon/apple-touch-icon.png','180x180',null,'apple-touch-icon');
+setFavicons('https://customapplications.github.io/k2.github.io/favicon/site.webmanifest',null,null,'manifest');
