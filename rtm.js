db23f760f13a5d2e40fbf820fd65f96a98cb5491
customapplications <r.celesti@mapei.it>
2025-12-10 11:53:49 +0100

    Add Favicon

--- rtm.js
+++ rtm.js
@@ -1,6 +1,19 @@
 $("span[name^=MapeiCell]").css("padding", "10px 15px");
 
-//$("div[name=MapeiTable] span[name^=Cell]").css("padding","5px 10px !important");
+function setFavicons(favImg){
+    let headTitle = document.querySelector('head');
+    let setFavicon = document.createElement('link');
+    setFavicon.setAttribute('rel','shortcut icon');
+    setFavicon.setAttribute('href',favImg);
+    headTitle.appendChild(setFavicon);
+}
+setFavicons('https://customapplications.github.io/k2.github.io/favicon/favicon.ico');
 
-console.log("a")
+/*
+<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+<link rel="shortcut icon" href="/favicon.ico" />
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+<link rel="manifest" href="/site.webmanifest" />
 
+*/
