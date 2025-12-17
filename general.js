712c67e788f890e802213dfafb87776d11c78eaa
customapplications <r.celesti@mapei.it>
2025-12-17 16:10:00 +0100

    favicon management

--- general.js
+++ general.js
@@ -5,6 +5,11 @@ $(document).ready(function() {
     renderGoogleFonts();
     appendPureCssClass();
     //addFormShadow()
+    setFavicons('https://customapplications.github.io/k2.github.io/favicon/favicon-96x96.png','96x96',null,'icon');
+setFavicons('https://customapplications.github.io/k2.github.io/favicon/favicon.svg',null,'image/svg+xml','icon');
+setFavicons('https://customapplications.github.io/k2.github.io/favicon/favicon.ico',null,null,'shortcut icon');
+setFavicons('https://customapplications.github.io/k2.github.io/favicon/apple-touch-icon.png','180x180',null,'apple-touch-icon');
+setFavicons('https://customapplications.github.io/k2.github.io/favicon/site.webmanifest',null,null,'manifest');
 });
 
 //drawLineBelowTitle()
@@ -101,3 +106,17 @@ const addFormShadow = () => {
 }
 
  */
+
+function setFavicons(favImg,size,type,rel){
+    let headTitle = document.querySelector('head');
+    let setFavicon = document.createElement('link');
+    setFavicon.setAttribute('rel',rel);
+    setFavicon.setAttribute('href',favImg);
+    if (size){
+        setFavicon.setAttribute('size',size);
+    }
+    if (type){
+        setFavicon.setAttribute('type',type);
+    }        
+    headTitle.appendChild(setFavicon);
+}
